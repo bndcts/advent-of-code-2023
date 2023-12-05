77832efae4c548e92719a9e907da75b1599680be
Benedikt Schwankner <benedikt_1212@outlook.de>
2023-12-05 19:15:22 +0100

    day 4 part2 done

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -6,11 +6,11 @@ fn main() {
     let file_path = "input.txt";
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
-    //let split_contents: Vec<Vec<char>> = contents.split("\n").collect::<Vec<Vec<char>>>();
     let prepared_data: Vec<&str> = contents
         .lines()
         .collect();
     first_part(&prepared_data);
+    second_part(&prepared_data);
 }
 
 fn prepare_data(data: &Vec<&str>) -> Vec<(HashSet<isize>, HashSet<isize>)>  {
@@ -20,7 +20,7 @@ fn prepare_data(data: &Vec<&str>) -> Vec<(HashSet<isize>, HashSet<isize>)>  {
         let first_part_only_cards_str = first_split[0].split(":").collect::<Vec<&str>>()[1];
 
         let first_part_only_cards_str_arr = first_part_only_cards_str.split(" ").collect::<Vec<&str>>();
-        
+
         let first_part : HashSet<isize> = first_part_only_cards_str_arr
             .into_iter()
             .filter_map(|x| x.parse::<isize>().ok())
@@ -55,4 +55,23 @@ fn first_part(raw_data: &Vec<&str>) {
         total +=score;
     }
     println!("First part: {}", total);
+}
+
+fn second_part(raw_data: &Vec<&str>) {
+    let data = prepare_data(raw_data);
+    let mut amount_of_cards : [usize; 205] = [1; 205];
+    for (n, (left, right)) in data.iter().enumerate() {
+        let mut score: isize = 0;
+        for i in left {
+            if right.contains(&i) {
+                score += 1;
+                }
+        }
+        for i in 1..score+1 {
+            let proper_i = i as usize;
+            amount_of_cards[n+proper_i] += amount_of_cards[n] * 1;
+        }
+    }
+    let score : usize = amount_of_cards.iter().sum();
+    println!("Second part: {}", score);
 }
