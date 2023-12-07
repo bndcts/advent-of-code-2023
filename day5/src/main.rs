20d2284e796f58241e93bee394b9948cad4c4f55
Benedikt Schwankner <benedikt_1212@outlook.de>
2023-12-07 21:43:36 +0100

    day 6 part 1 and 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     let prepared_data: Vec<&str> = contents
         .lines()
         .collect();
-    first_part(&prepared_data);
-    //second_part(&prepared_data);
+    //first_part(&prepared_data);
+    second_part(&prepared_data);
 }
 
 fn first_part(raw_data: &Vec<&str>) {
@@ -18,8 +18,71 @@ fn first_part(raw_data: &Vec<&str>) {
 }
 
 fn second_part(raw_data: &Vec<&str>) {
+    let mut min_location: usize = usize::MAX;
     let data = prepare_map_data(&raw_data);
     let seeds = find_seeds_part_two(&raw_data);
+    let mut seeds_ranges: Vec<(usize, usize)> = seeds;
+    for maps in &data {
+        let mut new_seed_ranges : Vec<(usize, usize)> = Vec::new();
+        while !seeds_ranges.is_empty() {
+            let mut to_add : Vec<(usize, usize)> = Vec::new();
+            for seed_range in &seeds_ranges {
+                for map in maps {
+                    let destination = map[0];
+                    let source = map[1];
+                    let range = map[2];
+                    if source >= seed_range.0 && source < seed_range.0 + seed_range.1 {
+                        let diff_source_seed1 = source - seed_range.0;
+                        let diff_source_end_seed_end = source + range -1 - (seed_range.0 + seed_range.1 -1);
+                        let new_seed_range = (destination+diff_source_seed_1, seed_range.1 + diff_source_end_seed_end);
+                        let new_old_seed_range_1 = (seed_range.0, diff_source_seed1);
+                        let new_old_seed_range_2 = (source + range-1, seed_range.1 - (source + range-1));
+                        if new_old_seed_range_1.1 > 0 {
+                            to_add.push(new_old_seed_range_1);
+                        }
+                        if new_old_seed_range_2.1 > 0 {
+                            to_add.push(new_old_seed_range_2);
+                        }
+                        new_seed_ranges.push(new_seed_range);
+                        to_add.push(new_seed_range);
+                    
+                    }
+                }
+            }
+            seeds_ranges = to_add;
+        }
+        seeds_ranges = new_seed_ranges;
+    }
+    for seed_range in &seeds_ranges {
+        if seed_range.0 < min_location {
+            min_location = seed_range.0;
+        }
+    }
+    println!("The minimum location is: {}", min_location);
+}
+
+fn find_seeds_part_two(raw_data: &Vec<&str>) -> Vec<(usize, usize)>{
+    let raw_line = raw_data[0];
+    let mut seeds: Vec<(usize, usize)> = Vec::new();
+    let mut current_source_seed = usize::MAX;
+    for word in raw_line.split_whitespace() {
+        if word.contains("seeds") {
+            continue;
+        }
+        if current_source_seed == usize::MAX {
+            current_source_seed = word.parse::<usize>().unwrap();
+            continue;
+        } else {
+            seeds.push((current_source_seed, word.parse::<usize>().unwrap()));
+            current_source_seed = usize::MAX;
+        }
+    }
+    return seeds;
+}
+
+fn second_part_brute_force(raw_data: &Vec<&str>) {
+    let data = prepare_map_data(&raw_data);
+    let seeds = find_seeds_part_two_brute_force(&raw_data);
     find_lowest_location(seeds, data);
 }
 
@@ -48,7 +111,7 @@ fn find_lowest_location (seeds: Vec<usize>, data: Vec<Vec<[usize; 3]>>) {
     println!("The minimum location is: {}", min_location);
 }
 
-fn find_seeds_part_two(raw_data : &Vec<&str>) -> Vec<usize>{
+fn find_seeds_part_two_brute_force(raw_data : &Vec<&str>) -> Vec<usize>{
     let raw_line = raw_data[0];
     let mut seeds: Vec<usize> = Vec::new();
     let mut current_source_seed = usize::MAX;
---
+++ day6/src/main.rs
@@ -0,0 +1,157 @@
+use std::fs;
+
+fn main() {
+    let file_path = "input.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    let prepared_data: Vec<&str> = contents
+        .lines()
+        .collect();
+    //first_part(&prepared_data);
+    //second_part_bin_search(&prepared_data);
+    part_two_dif(&prepared_data);
+}
+
+fn first_part(raw_data: &Vec<&str>) {
+    let mut data: Vec<Vec<&str>> = Vec::new();
+    for line in raw_data.iter() {
+        let split_line: Vec<&str> = line.split(" ").collect();
+        data.push(split_line[1..].to_vec());
+    }
+    let times : Vec<usize> = data[0].iter().filter_map(|x| x.parse::<usize>().ok()).collect();
+    let distances : Vec<usize> = data[1].iter().filter_map(|x| x.parse::<usize>().ok()).collect();
+    let mut total = 0;
+    for i in 0..times.len() {
+        let distance = distances[i];
+        let time = times[i];
+        let mut amount_of_possibilities = 0;
+        let mut max_reached = false;
+        for j in 1..time {
+            let covered_distance = (time-j) * j;
+            if covered_distance >= distance {
+                amount_of_possibilities += 1;
+                max_reached = true;
+            }
+            if max_reached && covered_distance < distance {
+                break;
+            }
+        }
+        if total == 0 {
+            total = amount_of_possibilities;
+        } else {
+            total *= amount_of_possibilities;
+        
+        }
+    }
+    println!("Total: {}", total);
+}
+
+fn second_part_brute_force(raw_data: &Vec<&str>) {
+    let time : usize = raw_data[0].split_whitespace().filter_map(|x| x.parse::<usize>().ok()).product();
+    let distance : usize = raw_data[1].split_whitespace().filter_map(|x| x.parse::<usize>().ok()).product();
+    println!("Time: {}, Distances: {}", time, distance);
+    let mut left = 1;
+    let mut right = time;
+    let mut result = 0;
+    while right > left {
+        let left_distance = (time - left) * left;
+        let right_distance = right * (time-right);
+        if left_distance < distance {
+            left += 1;
+        }
+        if right_distance < distance {
+            right -= 1;
+        }
+    }
+    let total = right - left;
+    println!("Total: {}", total);
+}
+
+fn second_part_bin_search(raw_data: &Vec<&str>) {
+    let time : usize = raw_data[0].split_whitespace().filter_map(|x| x.parse::<usize>().ok()).product();
+    let distance : usize = raw_data[1].split_whitespace().filter_map(|x| x.parse::<usize>().ok()).product();
+    let mut left = 1;
+    let mut right = time-1;
+    let mut left_mid = (left + right) / 2;
+    let mut right_mid = (left + right) / 2;
+    loop {
+        if left_go_up(left, time, distance) {
+            left = left + (left_mid-left)/2;
+        } else {
+            left_mid = left_mid - (left_mid-left)/2; 
+        }
+        if right_go_down(right, time, distance) {
+            right = right - (right-right_mid)/2;
+        } else {
+            right_mid = right_mid + (right-right_mid)/2;
+        }
+        if left == left_mid - 1 && right == right_mid + 1 {
+            let left_distance = (time - left) * left;
+            let right_distance = right * (time-right);
+            println!("time: {}, distance: {}", time, distance);
+            println!("Left: {}, Right: {}, Left Mid: {}, Right Mid: {}", left, right, left_mid, right_mid);
+            println!("Left Distance: {}, Right Distance: {}", left_distance, right_distance);
+            break;
+        }
+    }
+    let total = right - left;
+    println!("Total: {}", total);
+}
+
+fn part_two_dif(raw_data: &Vec<&str>) {
+    let time : usize = raw_data[0].split_whitespace().filter_map(|x| x.parse::<usize>().ok()).product();
+    let distance : usize = raw_data[1].split_whitespace().filter_map(|x| x.parse::<usize>().ok()).product();
+    println!("raw data: {:?}", raw_data);
+    let mut left = 1;
+    let mut right = time-1;
+    let mut mid = (left + right) / 2;
+    while !(is_enough(mid, time, distance) && !is_enough(mid+1, time, distance)){
+        if is_enough(mid, time, distance) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+        mid = (left + right) / 2;
+    }
+    let right_border = mid;
+    right = right_border;
+    left = 1;
+    mid = (left + right) / 2;
+    while !(is_enough(mid, time, distance) && !is_enough(mid-1, time, distance)){
+        if is_enough(mid, time, distance) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+        mid = (left + right) / 2;
+    }
+    println!("Right_botder:{} Mid: {}", right_border, mid);
+    println!("RIght distance: {}, Left Distance: {}", (time - right_border) * right_border, (time - mid) * mid);
+    println!("Min distance: {}", distance);
+    println!("right + 1 distance {}", (time - (right_border+1)) * (right_border+1));
+    println!("left -1 distance {}", (time - 15351) * 15351);
+    println!("Answer: {}", right_border - mid);
+}
+
+fn is_enough(point : usize, time : usize, distance : usize) -> bool {
+    if (time - point) * point >= distance {
+        true
+    } else {
+        false
+    }
+}
+fn left_go_up(left: usize, time:usize, distance:usize) -> bool {
+    if (time - left) * left < distance {
+        true
+    } else {
+        false
+    }
+}
+
+fn right_go_down(right:usize, time : usize, distance : usize) -> bool{
+    if right * (time - right) < distance {
+        true
+    } else {
+        false
+    }
+}
